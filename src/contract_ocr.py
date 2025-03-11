9b778bcc205d3ff7665772f91a289ceb34238298
Tim_Brown <timbrownhyz207@gmail.com>
2025-03-11 16:37:01 +0800

    Origin OCR

--- src/contract_ocr.py
+++ src/contract_ocr.py
@@ -1,5 +1,4 @@
 import os
-
 import fitz
 import cv2
 import numpy as np
@@ -15,6 +14,7 @@ def extract_text_from_pdf(pdf_path, ocr, pages_to_process, threshold=0.8):
     processed_results = []
     for page_idx, page_result in enumerate(result):
         if page_idx >= pages_to_process:
+            # 最后一页先不处理
             break
         if page_result is None:
             logging.debug(f"Empty page {page_idx + 1} detected, skip it.")
@@ -47,7 +47,7 @@ def save_ocr_results(result, imgs, output_dir):
 def process_pdf_ocr(pdf_path, output_dir):
     with fitz.open(pdf_path) as pdf:
         total_pages = pdf.page_count
-    pages_to_process = total_pages - 1  # 排除最后一页有印章的页面
+    pages_to_process = total_pages - 1  # 排除最后一页
 
     ocr = PaddleOCR(use_angle_cls=True, lang="ch", page_num=pages_to_process, use_gpu=0)
     processed_results = extract_text_from_pdf(pdf_path, ocr, pages_to_process, threshold=0.8)
@@ -67,14 +67,15 @@ def process_pdf_ocr(pdf_path, output_dir):
     save_ocr_results(result=ocr.ocr(pdf_path, cls=True), imgs=imgs, output_dir=output_dir)
     return processed_results
 
+
 if __name__ == '__main__':
     # Local Files
     input_dir = "../input/pdf/"
-    output_dir = '../output/contract/'
+    base_output_dir = '../output/contract/'
     for filename in os.listdir(input_dir):
         if filename.endswith(".pdf"):
             pdf_path = os.path.join(input_dir, filename)
-            output_dir = os.path.join(output_dir, f"{os.path.splitext(filename)[0]}")
-            os.makedirs(output_dir, exist_ok=True)
+            pdf_output_dir = os.path.join(base_output_dir, f"{os.path.splitext(filename)[0]}")
+            os.makedirs(pdf_output_dir, exist_ok=True)
             print(f"Processing: {pdf_path}")
-            process_pdf_ocr(pdf_path, output_dir)
+            process_pdf_ocr(pdf_path, pdf_output_dir)
